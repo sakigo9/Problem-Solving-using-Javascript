544cf780207642dedcca07929de550a9cdf6e379
sakigo9 <saketprag322@gmail.com>
2023-11-22 20:34:38 +0530

    modified solution for 2623

--- Problems/LeetCode/2623. Memoize/SolutionJS.js
+++ Problems/LeetCode/2623. Memoize/SolutionJS.js
@@ -10,3 +10,26 @@ function memoize(fn) {
         return result
     }
 }
+
+// Case 1 : Addition
+let checkCalls=0; // to check number of times this function is called
+function sum(a,b){
+    checkCalls+=1;
+    return a+b;
+}
+
+let instance=memoize(sum);
+instance(2,3);
+
+// Case 2: factorial
+
+function factorail(n){
+    checkCalls+=1;
+    if(n==0 || n==1){
+        return 1
+    }
+    return n * factorail(n-1);
+}
+
+let instance2 = memoize(factorail);
+instance2(7);
--- Problems/LeetCode/2623. Memoize/index.md
+++ Problems/LeetCode/2623. Memoize/index.md
@@ -1,10 +1,8 @@
 ### 2623. Memoize : https://leetcode.com/problems/memoize/description/
 
 ### JS Concepts Used :
-
+    * Memoizations
     * Closures
     * Higher Order Functions
 
-### Core Concept :
 
-    * Memoization
