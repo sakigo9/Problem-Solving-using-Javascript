cb1f06451322a759653e2423698c665cf3113ef5
sakigo9 <saketprag322@gmail.com>
2023-11-28 20:54:36 +0530

    added 2622 solution

---
+++ Problems/LeetCode/2622. Cache With Time Limit/2622.js
@@ -0,0 +1,45 @@
+var TimeLimitedCache = function () {
+  this.cache = new Map();
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  let toggle = false;
+  if (this.cache.has(key)) {
+    toggle = true;
+    clearTimeout(this.cache.get(key)[1]);
+  }
+  let setTimeoutInstance = setTimeout(() => {
+    this.cache.delete(key);
+  }, duration);
+  this.cache.set(key, [value, setTimeoutInstance]);
+  return toggle;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+  if (this.cache.has(key)) return this.cache.get(key)[0];
+  return -1;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
